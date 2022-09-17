3ae4ae549683374f61d325d9a81f6bf7e732f0d4
Laptop_main <mfranjic995@gmail.com>
2022-09-17 13:45:17 +0200

    SendGrid email sending implemented

--- pages/api/form.js
+++ pages/api/form.js
@@ -1,4 +1,8 @@
-export default function handler(req, res) {
+import sendgrid from '@sendgrid/mail';
+
+sendgrid.setApiKey('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');
+
+async function sendEmail(req, res) {
 	// Get data submitted in request's body.
 	const body = req.body;
 
@@ -6,14 +10,29 @@ export default function handler(req, res) {
 	// in the command line where next.js app is running.
 	console.log('body: ', body);
 
-	// Guard clause checks for first and last name,
-	// and returns early if they are not found
 	if (!body.firstName) {
 		// Sends a HTTP bad request error code
-		return res.status(400).json({ data: 'First name not found' });
+		//return res.status(400).json({ data: 'First name not found' });
 	}
 
 	// Found the name.
 	// Sends a HTTP success code
-	res.status(200).json({ data: `${body.firstName} ${body.secondName} ${body.title} ${body.email} ${body.message}` });
+	//res.status(200).json({ data: `${body.firstName} ${body.secondName} ${body.title} ${body.email} ${body.message}` });
+
+	try {
+		// console.log("REQ.BODY", req.body);
+		await sendgrid.send({
+			to: 'mfranjic995@gmail.com', // Your email where you'll receive emails
+			from: 'aziriontweb@gmail.com', // your website email address here
+			subject: `${body.message}`,
+			html: `<div>You've got a mail</div>`,
+		});
+	} catch (error) {
+		// console.log(error);
+		return res.status(error.statusCode || 500).json({ error: error.message });
+	}
+
+	return res.status(200).json({ error: '' });
 }
+
+export default sendEmail;
