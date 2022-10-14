d9f456f76a58b6beccaa7cf52e8597bbade105cf
Laptop_main <mfranjic995@gmail.com>
2022-10-14 23:37:50 +0200

    Thesis + Unity education projects data

--- data/projectsData.js
+++ data/projectsData.js
@@ -270,9 +270,9 @@ const projects = [
 		description: 'Aenean commodo ligula eget dolor. Aenean massa.',
 		time: '2019 - 2020',
 		tags: [['green', 'desktop']],
-		about: '',
-		idea: '',
-		result: '',
+		about: 'Couple of games of different genres with simple systems and mechanics',
+		idea: 'Quick group or solo projects to provide us with practical experience',
+		result: '3 different games, out of which 2 were playable on a game dev conference',
 		team: 'Education project team',
 		tools: ['Unity game engine', 'C#'],
 		youtube: 'https://www.youtube.com/embed/tHXp03DiiCg',
@@ -324,7 +324,7 @@ const projects = [
 			['blue', 'mobile'],
 			['red', 'AI'],
 		],
-		about: "Real game functionality that grew into a R&D project for master's thesis",
+		about: "Real game feature that grew into a R&D project for master's thesis",
 		idea: 'For a player to be able to draw a specific item in a comic book telltale game',
 		result: 'Convolutional neutral network that differentiates between 6 items',
 		team: "Solo, part of a student team's project",
@@ -333,24 +333,13 @@ const projects = [
 		timeline: '/resources/photos/projects/timeline-2.png',
 		description1: [
 			"At start of the academic year I graduated, I've joined a project with couple of students from both mine and other faculties. First goal we had was to make a simple comic book game in 2 months and present it on Reboot Infogamer, big gaming conference in Zagreb. At that time I've had zero knowledge or experience in Unity, so my task was a small one - to capture a screenshot of player's final appearance of the comic after the playthrough.",
-			'After that we decided to continue the development, but to expand the game both visually and mechanically. That next goal was to present the game on Reboot Develop in 5 months, which is the biggest conference of game dev industry in this part of Europe. I was the second Unity developer on the game, and since the other guy had way more skill and experience he took over the main task of game pipeline and the implementation of art and story. As an extra feature we wanted to add to the game was possibility for a player to be confronted with a blank panel with only art background - and for them to be able to draw an item which would then unlock the rest of the story! I had the opportunity to implement this feature.',
+			'After that we decided to continue the development, but to expand the game both visually and mechanically. That next goal was to present the game on Reboot Develop in 5 months, which is the biggest conference of game dev industry in this part of Europe. I was the second Unity developer on the game, and since the other guy had way more skill and experience he took over the main task of game pipeline and the implementation of art and story.',
+			'As an extra feature we wanted to add to the game was possibility for a player to be confronted with a blank panel with only art background - and for them to be able to draw an item which would then unlock the rest of the story! I had the opportunity to implement this feature.',
 		],
 		description2: [
-			'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula ' +
-				'eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis ' +
-				'parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies ' +
-				'nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. ' +
-				'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim ' +
-				'justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis ' +
-				'eu pede mollis pretium. Integer tincidunt.',
-			'Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. ' +
-				'Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ' +
-				'ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut ' +
-				'metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel ' +
-				'augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas ' +
-				'tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing ' +
-				'sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, ' +
-				'lorem. Maecenas nec odio et ante tincidunt tempus.',
+			"So, how did this interesting feature on a non-university project become a focal point of my master's thesis? I have no idea, honestly. I had been doing internship at the same time, but this project motivated me more to learn and research something new. That's way I've decided to pitch this object recognition idea to my mentor, and it was successful. ",
+			"For it to be compatible with our game, I've decided to fully implement all of the neural network componenents through Unity. I was aware that there are AI tools and plugins that already exist, but at that time I couldn't figure out how to connect any of them with our game. So I gave up after couple of weeks (mostly because of project's deadlines approaching), and decided to build everything for the ground up. ",
+			"After building and connecting all the necesarry network layers, I've constructed a special Unity scene to train the network for those 6 specific items that we had for players to draw in the game. Different variants of training results were used both to optimize the game feature, and in the thesis to create certain conclussions. ",
 		],
 	},
 	//#endregion
@@ -370,11 +359,13 @@ const projects = [
 		youtube: 'https://www.youtube.com/embed/tHXp03DiiCg',
 		timeline: '/resources/photos/projects/timeline-1.png',
 		description1: [
-			"Back then I still had no idea what I wanted to do with my career as a software developer. Having finished all my university exams I started looking for an internship to get some real life experience. This was the first interview that I've ever done in my life, and it was with one of the biggest game development companies in Zagreb. They were searching for an intern for the summer months, and I decided it could be a nice opportunity to apply. Except for coding skills, my formal education didn't really provide me with any knowledge or insight of game dev industry. I figured that my lack of any experience could be a slight issue, but it was worth a try!",
+			"Back then I still had no idea what I wanted to do with my career as a software developer. Having finished all my university exams I started looking for an internship to get some real life experience. This was the first interview that I've ever done in my life, and it was with one of the biggest game development companies in Zagreb.",
+			"They were searching for an intern for the summer months, and I decided it could be a nice opportunity to apply. Except for coding skills, my formal education didn't really provide me with any knowledge or insight of game dev industry. I figured that my lack of any experience could be a slight issue, but it was worth a try!",
 			"After the HR interview they sent me the assignment for which I had 7 days to complete. The main focus was that the game wasn't built using a game engine and is fully functional, and even though there were lots of small extra tasks, how the game looked visually was not important. What was also importat though, was the extensibility and flexibility of different parts of the game.",
 		],
 		description2: [
-			"Even before writing a single line of code I've decided to use paper and pencil to sketch basically anything that I could think would be useful. I've decided on  and drew all functionalities, systems, and extensibility methods. I've really enjoyed this practice, and is even nowadays one of my prefered methods of starting a project. Alright, maybe not with paper and pencil anymore. The result was pretty good, and I've implemented successfully everything that I had in plan. My main downfall on the interview ended up being not knowing C# and databases more in-depth, and probably a bit too much of spaghetti code.",
+			"Even before writing a single line of code I've decided to use paper and pencil to sketch basically anything that I could think would be useful. I've decided on  and drew all functionalities, systems, and extensibility methods. I've really enjoyed this practice, and is even nowadays one of my prefered methods of starting a project. Alright, maybe not with paper and pencil anymore.",
+			"The result was pretty good, and I've implemented successfully everything that I had in plan. My main downfall on the interview ended up being not knowing C# and databases more in-depth, and probably a bit too much of spaghetti code.",
 			"As for game's frontend, since I had no idea about anything else that I can learn and use in 1 week, I've decided to go with .NET's Window Forms. I had some experience using .NET during university projects. I knew there are probably tons of better and more optimized options for a quick game like this one, but for this specific situation and timeframe I belive it was the right call.",
 		],
 	},
